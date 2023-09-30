7958f672e4c36b45459c52f13fd99035645c5300
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-10-01 08:35:31 +0900

    defined git-dir? and git-dirs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,14 @@
 
 ## Unreleased
 
+## 0.1.1 - 2023-10-01
+- git flow init
+- git-dir?
+- git-dirs
+
 ## 0.1.0 - 2023-10-01
 - initialized repository
 
     git init
     gh repo create hkim0331/gits --public
-    git remote add ssh://github.com/hkim0331/gits.git
+    git remote add ssh://git@github.com/hkim0331/gits.git
--- bb/gits.clj
+++ bb/gits.clj
@@ -32,8 +32,34 @@
   "ディレクトリを引数に取り、git verb を実行する関数を返す。"
   [verb]
   (fn [dir]
-    (ps/shell {:dir dir} (str "git " verb))))
+    (-> (ps/shell {:dir dir :out :string :err :string}
+                  (str "git " verb))
+        :out)))
 
+(comment
+  ((git "status") ".")
+  :rcf)
+
+(defn git-dir?
+  "dir が git 配下かを dir/.git が存在するかで判定する。"
+  [dir]
+  (fs/exists? (str dir "/.git")))
+
+(comment
+  (git-dir? ".")
+  (git-dir? "/Users/hkim/ramdisk")
+  :rcf)
+
+(defn git-dirs
+  "dir 以下の subdir で、git 配下のディレクトリだけを返す。"
+  [dir]
+  (filter git-dir? (fs/list-dir (fs/expand-home dir))))
+
+(comment
+  (git-dirs "/Users/hkim")
+  (git-dirs "/Users/hkim/projects")
+  (git-dirs "~/projects")
+  :rcf)
 
 (defn gits
   ([] (gits "."))
